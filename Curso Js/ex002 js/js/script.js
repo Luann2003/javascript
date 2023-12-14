43e492014e12c17b29ebdcdeb5920e80eaf526d2
Luann2003 <lvictorluciano@outlook.com>
2023-12-14 10:36:19 -0300

    update male or femelae

--- Curso Js/ex002 js/js/script.js
+++ Curso Js/ex002 js/js/script.js
@@ -1,14 +1,32 @@
 function buscar(){
     var inp = document.getElementById('input').value; 
     var img = document.getElementById('imagem');
-    
-    var masculino = document.getElementById("male");
-    var feminino = document.getElementById("female");
+    var res = document.getElementById('res');
+    var data = new Date();
+    var ano = data.getFullYear();
 
-    if(parseInt(inp) <= 2000 && masculino.checked){
-        img.src = "imagens/tarde.png"
+   if(inp > ano){
+    window.alert(`[ERRO] verifique os dados e tente novamente`);
+   }else{
+    var sexo = document.getElementsByName('sexo')
+    var idade = ano - inp;
+    res.innerHTML = `idade calculada ${idade}`
+    if(sexo[0].checked){
+        if(inp < 1990){
+            img.src = 'imagens/tarde.png'
+        }
+        else if(inp < 2000){
+            img.src = 'imagens/manha.png'
+        }
+
+    }else if(sexo[1].checked) {
+        if(inp < 1990){
+            img.src = 'imagens/tarde.png'
+        }
+        else if(inp < 2000){
+            img.src = 'imagens/manha.png'
+        }
     }
-    else if(parseInt(inp) <= 2000 && feminino.checked){
-        img.src = "imagens/night.png"
-    }
+   }
+    
 }
