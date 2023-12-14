c33eb5045293fec419a2b2dac0cf8bd81768dc73
Luann2003 <lvictorluciano@outlook.com>
2023-12-14 11:54:46 -0300

    update

---
+++ Curso Js/ex003 js/js/script.js
@@ -0,0 +1,14 @@
+var inicio = document.getElementById('inicio').value;
+var fim = document.getElementById('fim').value;
+var passo = document.getElementById('passo').value;
+var res = document.getElementById('res');
+
+function contar(){
+    res = 'aaa'
+    while(inicio < fim){
+        inicio + passo
+
+        //res = `${inicio} >`
+    }
+
+}
